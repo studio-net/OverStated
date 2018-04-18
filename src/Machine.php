2fdd58f4fda43acf63f915348e63738d3a67a669
Olivier <763289+kadmelia@users.noreply.github.com>
2018-04-18 14:16:33 +0200

    changes indentation method

--- src/Machine.php
+++ src/Machine.php
@@ -6,324 +6,320 @@ use OverStated\Exceptions\UninitialisedException;
 use OverStated\States\State;
 use Illuminate\Database\Eloquent\Model;
 
-
 /**
  * Class Machine
- * @package FSM
- */
+* @package FSM
+*/
 class Machine
 {
-    /**
-     * The name of the state machine
-     *
-     * @var string
-     */
-    protected $id;
+   /**
+    * The name of the state machine
+    *
+    * @var string
+    */
+   protected $id;
 
-    /**
-     * @var EventInterface
-     */
-    protected $events;
+   /**
+    * @var EventInterface
+    */
+   protected $events;
 
-    /**
-     * The state structure
-     *
-     * @var StructureInterface
-     */
-    private $structure;
+   /**
+    * The state structure
+    *
+    * @var StructureInterface
+    */
+   private $structure;
 
-    /**
-     * List of historical transitions
-     *
-     * @var array
-     */
-    private $history = [];
+   /**
+    * List of historical transitions
+    *
+    * @var array
+    */
+   private $history = [];
 
-    /**
-     * The active state
-     *
-     * @var State
-     */
-    private $state;
+   /**
+    * The active state
+    *
+    * @var State
+    */
+   private $state;
 
-    /**
-     * Construct the machine
-     *
-     * @param EventInterface $events
-     */
-    public function __construct(EventInterface $events)
-    {
-        $this->events = $events;
+   /**
+    * Construct the machine
+    *
+    * @param EventInterface $events
+    */
+   public function __construct(EventInterface $events)
+   {
+      $this->events = $events;
+      $this->listen('initialise', [$this, 'initialise']);
+   }
 
-        $this->listen('initialise', [$this, 'initialise']);
-    }
+   /**
+    * Set the initial state manually
+    *
+    * @param null $state
+    */
+   public function initialise($state = null)
+   {
+      $this->state = $this->structure->getInitialState($state);
 
-    /**
-     * Set the initial state manually
-     *
-     * @param null $state
-     */
-    public function initialise($state = null)
-    {
-        $this->state = $this->structure->getInitialState($state);
+      $this->forget('initialise');
 
-        $this->forget('initialise');
+   }
 
-    }
+   /**
+    * Transition from the current state to another via a valid
+    * transition.
+    *
+    * @param string $transition
+    * @return bool
+    * @throws UninitialisedException
+    */
+   public function transition($transition)
+   {
+      $transition = $this->structure->getTransition($transition);
+      $destState = $transition->getTo();
 
-    /**
-     * Transition from the current state to another via a valid
-     * transition.
-     *
-     * @param string $transition
-     * @return bool
-     * @throws UninitialisedException
-     */
-    public function transition($transition)
-    {
-        $transition = $this->structure->getTransition($transition);
-        $destState = $transition->getTo();
-
-        if (! $this->state) {
+      if (! $this->state) {
             throw new UninitialisedException('FSM is not initialised.');
-        }
+      }
 
-        $currentState = $this->getState()->getId();
+      $currentState = $this->getState()->getId();
 
-        if (!$this->structure->canTransitionFrom($currentState, $destState)) {
+      if (!$this->structure->canTransitionFrom($currentState, $destState)) {
             throw new Exceptions\TransitionException(
-                sprintf(
-                    "Transition '%s' to status '%s' is impossible from status '%s'",
-                    $transition->getSlug(),
-                    $destState,
-                    $this->getState()->getId()
-                )
+               sprintf(
+                  "Transition '%s' to status '%s' is impossible from status '%s'",
+                  $transition->getSlug(),
+                  $destState,
+                  $this->getState()->getId()
+               )
             );
-        }
+      }
 
-        $transition->onTransit();
+      $transition->onTransit();
 
-        array_push($this->history, $currentState);
-        $this->setState($this->structure->getState($destState));
+      array_push($this->history, $currentState);
+      $this->setState($this->structure->getState($destState));
 
-        // Emit a transition event
-        $this->fire("transition", [
-            "from"       => $this->structure->getState($currentState),
-            "transition" => $transition
-        ]);
+      // Emit a transition event
+      $this->fire("transition", [
+         "from"       => $this->structure->getState($currentState),
+         "transition" => $transition
+      ]);
 
-    }
+   }
 
-
-
-    /**
-     * Handle a method on the current state.
-     *
-     * @param string $handle
-     * @param array $args
-     * @return mixed
-     *
-     * @throws UninitialisedException
-     */
-    public function handle($handle, $args = [])
-    {
-        if (! $this->state) {
+   /**
+    * Handle a method on the current state.
+    *
+    * @param string $handle
+    * @param array $args
+    * @return mixed
+    *
+    * @throws UninitialisedException
+    */
+   public function handle($handle, $args = [])
+   {
+      if (! $this->state) {
             throw new UninitialisedException('FSM is not initialised.');
-        }
+      }
 
-        array_unshift($args, $this->state);
+      array_unshift($args, $this->state);
 
-        $data = $this->execHandle($handle, $args);
+      $data = $this->execHandle($handle, $args);
 
-        $this->fire("handled.{$handle}", [$this->state, $data]);
+      $this->fire("handled.{$handle}", [$this->state, $data]);
 
-        return $data;
-    }
+      return $data;
+   }
 
-    /**
-     * Execute the handler on the state
-     *
-     * @param $handle
-     * @param array $args
-     *
-     * @return mixed
-     */
-    protected function execHandle($handle, $args = [])
-    {
-        $result = call_user_func_array([$this->state, $handle], $args);
+   /**
+    * Execute the handler on the state
+    *
+    * @param $handle
+    * @param array $args
+    *
+    * @return mixed
+    */
+   protected function execHandle($handle, $args = [])
+   {
+      $result = call_user_func_array([$this->state, $handle], $args);
 
-        return is_null($result) ? : $result;
-    }
+      return is_null($result) ? : $result;
+   }
 
-    /**
-     * Get the history array
-     *
-     * @return array
-     */
-    public function getHistory()
-    {
-        return $this->history;
-    }
+   /**
+    * Get the history array
+    *
+    * @return array
+    */
+   public function getHistory()
+   {
+      return $this->history;
+   }
 
-    /**
-     * Get a list of all possible state transitions.
-     *
-     * @param $options array
-     * @return array
-     */
-    public function getTransitions($options = [])
-    {
-        $options = $options + [
+   /**
+    * Get a list of all possible state transitions.
+    *
+    * @param $options array
+    * @return array
+    */
+   public function getTransitions($options = [])
+   {
+      $options = $options + [
             "onlyValid"   => false,
-        ];
+      ];
 
-        $transitions = $this->structure->getTransitionsFrom(
+      $transitions = $this->structure->getTransitionsFrom(
             $this->getState()->getId());
 
-        $validTransitions = [];
-        foreach ($transitions as $transition) {
+      $validTransitions = [];
+      foreach ($transitions as $transition) {
             if ($transition->canTransit()) {
-                $validTransitions[] = $transition;
+               $validTransitions[] = $transition;
             }
-        }
+      }
 
-        return $options["onlyValid"] ? $validTransitions : $transitions;
-    }
+      return $options["onlyValid"] ? $validTransitions : $transitions;
+   }
 
-    /**
-     * Get the current state instance
-     *
-     * @return State
-     */
-    public function getState()
-    {
-        return $this->state;
-    }
+   /**
+    * Get the current state instance
+    *
+    * @return State
+    */
+   public function getState()
+   {
+      return $this->state;
+   }
 
-    /**
-     * Set the current state
-     *
-     * @param State $state
-     */
-    public function setState(State $state)
-    {
-        $this->state = $state;
-    }
+   /**
+    * Set the current state
+    *
+    * @param State $state
+    */
+   public function setState(State $state)
+   {
+      $this->state = $state;
+   }
 
-    /**
-     * Set associated model.
-     * @param Model $model
-     */
-    public function setModel(Model $model)
-    {
-        $this->model = $model;
-    }
+   /**
+    * Set associated model.
+    * @param Model $model
+    */
+   public function setModel(Model $model)
+   {
+      $this->model = $model;
+   }
 
-    /**
-     * Get associated model.
-     *
-     * @return Model
-     */
-    public function getModel() : Model
-    {
-        return $this->model;
-    }
+   /**
+    * Get associated model.
+    *
+    * @return Model
+    */
+   public function getModel() : Model
+   {
+      return $this->model;
+   }
 
-    /**
-     * Get the machines structure instance.
-     *
-     * @return mixed
-     */
-    public function getStructure()
-    {
-        return $this->structure;
-    }
+   /**
+    * Get the machines structure instance.
+    *
+    * @return mixed
+    */
+   public function getStructure()
+   {
+      return $this->structure;
+   }
 
-    /**
-     * @param StructureInterface $structure
-     */
-    public function setStructure(StructureInterface $structure)
-    {
-        $this->structure = $structure;
-    }
+   /**
+    * @param StructureInterface $structure
+    */
+   public function setStructure(StructureInterface $structure)
+   {
+      $this->structure = $structure;
+   }
 
-    /**
-     * Get the machines id
-     *
-     * @return string
-     */
-    public function getId()
-    {
-        if (isset($this->id)) {
+   /**
+    * Get the machines id
+    *
+    * @return string
+    */
+   public function getId()
+   {
+      if (isset($this->id)) {
             return $this->id;
-        }
+      }
 
-        return $this->id = str_replace('\\', '', strtolower(class_basename($this)));
-    }
+      return $this->id = str_replace('\\', '', strtolower(class_basename($this)));
+   }
 
-    /**
-     * Set the machines ID
-     *
-     * @param $id
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
+   /**
+    * Set the machines ID
+    *
+    * @param $id
+    */
+   public function setId($id)
+   {
+      $this->id = $id;
+   }
 
 
-    /**
-     * Fire an event bound to this machines ID.
-     *
-     * @param $type
-     * @param array $args
-     */
-    public function fire($type, $args = [])
-    {
-        array_unshift($args, $this);
+   /**
+    * Fire an event bound to this machines ID.
+    *
+    * @param $type
+    * @param array $args
+    */
+   public function fire($type, $args = [])
+   {
+      array_unshift($args, $this);
 
-        $this->events->fire($this->appendId($type), $args);
-    }
+      $this->events->fire($this->appendId($type), $args);
+   }
 
-    /**
-     * Listen for events of a type bound to this machines ID.
-     *
-     * @param $type
-     * @param callable $closure
-     */
-    public function listen($type, callable $closure)
-    {
-        $this->events->listen($this->appendId($type), $closure);
-    }
+   /**
+    * Listen for events of a type bound to this machines ID.
+    *
+    * @param $type
+    * @param callable $closure
+    */
+   public function listen($type, callable $closure)
+   {
+      $this->events->listen($this->appendId($type), $closure);
+   }
 
-    /**
-     * Forget an event or events that were previously
-     * registered.
-     *
-     * @param $events
-     */
-    public function forget($events)
-    {
-        if (is_array($events)) {
+   /**
+    * Forget an event or events that were previously
+    * registered.
+    *
+    * @param $events
+    */
+   public function forget($events)
+   {
+      if (is_array($events)) {
             foreach ($events as $event) {
-                $events = $this->appendId($event);
+               $events = $this->appendId($event);
             }
-        } else {
+      } else {
             $events = $this->appendId($events);
-        }
+      }
 
-        $this->events->forget($events);
-    }
+      $this->events->forget($events);
+   }
 
-    /**
-     * Append the machines ID to a string using
-     * dot notation.
-     *
-     * @param $string
-     * @return string
-     */
-    protected function appendId($string)
-    {
-        return "{$this->getId()}.{$string}";
-    }
+   /**
+    * Append the machines ID to a string using
+    * dot notation.
+    *
+    * @param $string
+    * @return string
+    */
+   protected function appendId($string)
+   {
+      return "{$this->getId()}.{$string}";
+   }
 }
--- src/Support/Stateful.php
+++ src/Support/Stateful.php
@@ -5,69 +5,67 @@ use OverStated\Builders\GraphBuilder;
 
 /**
  * Stateful
- *
- * @see BaseModel
  */
 trait Stateful {
 
-	/** @var Machine[] $fsms */
-	protected $fsms = [];
+   /** @var Machine[] $fsms */
+   protected $fsms = [];
 
-	/**
-	 * Create FSMs to manage FSM-based-fields.
-	 */
-	private function registerFsms() {
+   /**
+    * Create FSMs to manage FSM-based-fields.
+    */
+   private function registerFsms() {
 
-		if (empty($this->fsmDefinitions)) {
-			throw new \Exception("FSM not defined.");
-		}
+      if (empty($this->fsmDefinitions)) {
+         throw new \Exception("FSM not defined.");
+      }
 
-		foreach ($this->fsmDefinitions as $field => $fsmInfos) {
-			$builder = new GraphBuilder();
-			$builder = $builder->create();
+      foreach ($this->fsmDefinitions as $field => $fsmInfos) {
+         $builder = new GraphBuilder();
+         $builder = $builder->create();
 
-			foreach ($fsmInfos['states'] as $state) {
-				$builder->state($state);
-			}
-			foreach ($fsmInfos['transitions'] as $transition) {
-				$builder->transition($transition);
-			}
-			$builder->model($this);
-			$this->fsms[$field] = $builder->getMachine();
+         foreach ($fsmInfos['states'] as $state) {
+            $builder->state($state);
+         }
+         foreach ($fsmInfos['transitions'] as $transition) {
+            $builder->transition($transition);
+         }
+         $builder->model($this);
+         $this->fsms[$field] = $builder->getMachine();
 
-			if (!empty($this->$field)) {
-				$this->fsms[$field]->initialise($this->status);
-			}
-		}
+         if (!empty($this->$field)) {
+            $this->fsms[$field]->initialise($this->status);
+         }
+      }
 
-	}
+   }
 
-	/**
-	 * Get requested FSM.
-	 *
-	 * @param string $field
-	 * @return OverStated\Machine
-	 */
-	public function getFsm($field) : \OverStated\Machine {
+   /**
+    * Get requested FSM.
+    *
+    * @param string $field
+    * @return OverStated\Machine
+    */
+   public function getFsm($field) : \OverStated\Machine {
 
-		if (empty($this->fsms)) {
-			$this->registerFsms();
-		}
-		return $this->fsms[$field];
-	}
+      if (empty($this->fsms)) {
+         $this->registerFsms();
+      }
+      return $this->fsms[$field];
+   }
 
-	/**
+   /**
     * Transit from the current state to another via a valid
-	 * transition.
-	 *
-	 * @throws \Exception if cant transit.
-	 * @param string $field
-	 * @param string $transition
-	 */
-	public function fsmTransit($field, $transition) {
-		$this->getFsm($field)->transition($transition);
-		$this->$field = $this->getFsm($field)->getState()->getId();
-		$this->save();
-	}
+   * transition.
+   *
+   * @throws \Exception if cant transit.
+   * @param string $field
+   * @param string $transition
+   */
+   public function fsmTransit($field, $transition) {
+      $this->getFsm($field)->transition($transition);
+      $this->$field = $this->getFsm($field)->getState()->getId();
+      $this->save();
+   }
 
 }
